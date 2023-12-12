96c8e1688ea15300c302e700e7a261751adc0e25
Limonchikus <147668063+Limonchikus@users.noreply.github.com>
2023-12-12 23:00:07 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 
 def main():
-    print("Привет, это Limonchik, и это мой эксперимент со временем в Docker!")
+    print("Привет, это Limonchikus, и это мой эксперимент со временем в Docker!")
     print(f"Текущее время и дата: {datetime.datetime.now()}")
 
 if __name__ == "__main__":
